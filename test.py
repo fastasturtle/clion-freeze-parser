ae8b90058270ffb149df5f95fa40cf9e20e54f88
Semyon Kolton <semyon.kolton@jetbrains.com>
2019-08-02 13:04:39 +0300

    Fix several tests

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def run_tests():
         with open(gold_name) as gold_file:
             result = parse_args_and_process_files([file_name])
             gold = gold_file.readlines()
-            if "".join(result).strip() != "".join(gold).strip():
+            if "".join(result).strip() != prepare_gold(gold):
                 with open(out_name, "w") as out_file:
                     out_file.writelines(result)
                 print(f + " failed")
@@ -28,6 +28,10 @@ def run_tests():
                 if overwrite:
                     print("Overwriting...")
                     os.rename(out_name, gold_name)
+
+
+def prepare_gold(gold):
+    return "".join(filter(lambda g: g != "\n", gold)).strip()
 
 
 if __name__ == '__main__':
