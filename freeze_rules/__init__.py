a92a00f452e57a532436e71e55a27132ff4fed7f
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2019-10-03 18:18:01 +0300

    Better handling rules when creating new rule file

--- freeze_rules/__init__.py
+++ freeze_rules/__init__.py
@@ -1,3 +1,6 @@
+import pkgutil
+import os
+
 import freeze_rules.actions
 import freeze_rules.commit
 import freeze_rules.lazyReparse
@@ -9,3 +12,10 @@ import freeze_rules.clangd
 import freeze_rules.debugger
 
 import freeze_rules.fixed
+
+
+def get_rules():
+    rules = []
+    for (i, name, _) in pkgutil.iter_modules([os.path.dirname(__file__)]):
+        rules += eval(name + '.get_rules()')
+    return rules
